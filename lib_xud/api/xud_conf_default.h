e4083c6bc9196feb2166f4f665e149b99fe3e259
xross <ross@xmos.com>
2017-10-16 17:26:30 +0100

    File move and add

---
+++ lib_xud/api/xud_conf_default.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2017, XMOS Ltd, All rights reserved
+
+#ifndef __XUD_CONF_DEFAULT_H__
+#define __XUD_CONF_DEFAULT_H__
+
+#ifdef __xud_conf_h_exists__
+    #include "xud_conf.h"
+#endif
+
+#ifndef __XUD_CONF_DEFAULT_H__
+#endif

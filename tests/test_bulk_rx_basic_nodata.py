b18bf4e789693112c328183d87c4c9257c15bd97
xross <ross@xmos.com>
2019-11-05 11:14:01 +0000

    Added test_bulk_rx_basic_nodata

---
+++ tests/test_bulk_rx_basic_nodata.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+# Rx out of seq (but valid.. ) data PID
+
+import random
+import xmostest
+from  usb_packet import *
+from usb_clock import Clock
+from helpers import do_usb_test, runall_rx
+
+
+# Single, setup transaction to EP 0
+
+def do_test(arch, clk, phy, seed):
+    rand = random.Random()
+    rand.seed(seed)
+
+    address = 1
+    ep = 1
+
+    # The inter-frame gap is to give the DUT time to print its output
+    packets = []
+
+    dataval = 0;
+
+    ipg = 500
+
+    AppendOutToken(packets, ep, address)
+    packets.append(TxDataPacket(rand, data_start_val=dataval, length=10, pid=0x3)) #DATA0
+    packets.append(RxHandshakePacket())
+    
+    dataval += 10
+    AppendOutToken(packets, ep, address, inter_pkt_gap=ipg)
+    packets.append(TxDataPacket(rand, data_start_val=dataval, length=11, pid=0xb)) #DATA1
+    packets.append(RxHandshakePacket())
+
+    AppendOutToken(packets, ep, address, inter_pkt_gap=ipg)
+    # MISSING DATA PAYLOAD
+
+
+    dataval += 11
+    AppendOutToken(packets, ep, address, inter_pkt_gap=ipg)
+    packets.append(TxDataPacket(rand, data_start_val=dataval, length=12, pid=0x3)) #DATA0
+    packets.append(RxHandshakePacket())
+
+    dataval += 12
+    AppendOutToken(packets, ep, address, inter_pkt_gap=ipg)
+    packets.append(TxDataPacket(rand, data_start_val=dataval, length=13, pid=0xb)) #DATA1
+    packets.append(RxHandshakePacket())
+
+    dataval += 13
+    AppendOutToken(packets, ep, address, inter_pkt_gap=ipg)
+    packets.append(TxDataPacket(rand, data_start_val=dataval, length=14, pid=0x3)) #DATA0
+    packets.append(RxHandshakePacket())
+
+    do_usb_test(arch, clk, phy, packets, __file__, seed, level='smoke', extra_tasks=[])
+
+def runtest():
+    random.seed(1)
+    runall_rx(do_test)
---
+++ tests/test_bulk_rx_basic_nodata/src/main.xc
@@ -0,0 +1,67 @@
+// Copyright (c) 2018-2019, XMOS Ltd, All rights reserved
+#include <xs1.h>
+#include <print.h>
+#include <stdio.h>
+#include "xud.h"
+#include "platform.h"
+#include "shared.h"
+
+#define XUD_EP_COUNT_OUT   5
+#define XUD_EP_COUNT_IN    5
+
+#ifndef PKT_LENGTH_START
+#define PKT_LENGTH_START 10
+#endif
+
+#ifndef PKT_LENGTH_END
+#define PKT_LENGTH_END 14
+#endif
+
+#ifndef TEST_EP_NUM
+#define TEST_EP_NUM   1
+#endif
+
+/* Endpoint type tables */
+XUD_EpType epTypeTableOut[XUD_EP_COUNT_OUT] = {XUD_EPTYPE_CTL, XUD_EPTYPE_BUL, XUD_EPTYPE_ISO, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL};
+XUD_EpType epTypeTableIn[XUD_EP_COUNT_IN] =   {XUD_EPTYPE_CTL, XUD_EPTYPE_BUL, XUD_EPTYPE_ISO, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL};
+
+#ifdef XUD_SIM_RTL
+int testmain()
+#else
+int main()
+#endif
+{
+    chan c_ep_out[XUD_EP_COUNT_OUT], c_ep_in[XUD_EP_COUNT_IN];
+            
+    par
+    {
+        { 
+            #if defined(XUD_TEST_SPEED_FS)
+            unsigned speed = XUD_SPEED_FS;
+            #elif defined(XUD_TEST_SPEED_HS)
+            unsigned speed = XUD_SPEED_HS;
+            #else
+            #error XUD_TEST_SPEED_XX not defined
+            #endif
+
+            XUD_Main(c_ep_out, XUD_EP_COUNT_OUT, c_ep_in, XUD_EP_COUNT_IN,
+                null, epTypeTableOut, epTypeTableIn,
+                null, null, -1, speed, XUD_PWR_BUS);
+        }
+
+        {
+            unsigned fail = TestEp_Rx(c_ep_out[TEST_EP_NUM], TEST_EP_NUM, PKT_LENGTH_START, PKT_LENGTH_END);
+
+            if(fail)
+                TerminateFail(fail);
+            else
+                TerminatePass(fail);    
+            
+            XUD_ep ep0 = XUD_InitEp(c_ep_out[0]);
+            XUD_Kill(ep0);
+            exit(0);
+        }
+    }
+
+    return 0;
+}

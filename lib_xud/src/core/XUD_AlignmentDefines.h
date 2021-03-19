d2d3622381808a470a393e172ca485aff7aaacb3
Dave Atkinson <davidatkinson@xmos.com>
2021-03-19 13:52:24 +0000

    Fix for XS3 function alignment issue.

---
+++ lib_xud/src/core/XUD_AlignmentDefines.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2021, XMOS Ltd, All rights reserved
+/* XUD_AlignmentDefines.h
+ * @brief Architecture-specific ASM function alignment
+ */
+
+#ifndef _XUD_ALIGNMENT_DEFINES_
+#define _XUD_ALIGNMENT_DEFINES_
+#if defined(__XS3A__)
+#define IBUFFER_FETCH_CORRECTION 1
+#elif defined(__XS2A__)
+#define IBUFFER_FETCH_CORRECTION 0
+#else
+#error No architecture defined
+#endif
+
+#if IBUFFER_FETCH_CORRECTION == 1
+#define FUNC_ALIGN4 16
+#define FUNC_ALIGN8 16
+#elif IBUFFER_FETCH_CORRECTION == 0
+#define FUNC_ALIGN4 4
+#define FUNC_ALIGN8 8
+#else
+#error IBUFFER_FETCH_CORRECTION not defined
+#endif
+
+#endif // _XUD_ALIGNMENT_DEFINES_

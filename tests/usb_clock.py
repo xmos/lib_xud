123e901187bea8c48d0d35fb7a8adb91d9780f71
Dave Atkinson <davidatkinson@xmos.com>
2021-04-13 14:34:14 +0100

    Automatic update of python 2 to 3.

--- tests/usb_clock.py
+++ tests/usb_clock.py
@@ -49,7 +49,7 @@ class Clock(xmostest.SimThread):
         return self._bit_time
 
     def stop(self):
-        print "**** CLOCK STOP ****"
+        print("**** CLOCK STOP ****")
         self._running = False
 
     def start(self):

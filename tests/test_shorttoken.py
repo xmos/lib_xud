a0e3f7bb86f8b2284c45de14dda3816b4e0b00a4
xross <ross@xmos.com>
2021-04-15 18:55:03 +0100

    Fixes to test_shorttoken. Note, expected fail on XS3

--- tests/test_shorttoken.py
+++ tests/test_shorttoken.py
@@ -1,47 +1,30 @@
 #!/usr/bin/env python
-
-# Same as simple RX bulk test but some invalid tokens also included
-
-import random
 import xmostest
-from  usb_packet import *
-from usb_clock import Clock
-from helpers import do_usb_test, runall_rx
+from usb_packet import TxPacket, USB_PID 
+from helpers import do_usb_test, RunUsbTest
+from usb_session import UsbSession
+from usb_transaction import UsbTransaction
 
-
-# Single, setup transaction to EP 0
-
-def do_test(arch, clk, phy, data_valid_count, usb_speed, seed):
-    rand = random.Random()
-    rand.seed(seed)
+def do_test(arch, clk, phy, data_valid_count, usb_speed, seed, verbose=False):
 
     address = 1
     ep = 1
 
-    packets = []
-    dataval = 0;
-
+    session = UsbSession(bus_speed=usb_speed, run_enumeration=False, device_address = address)
 
     # Start with a valid transaction */
-    AppendOutToken(packets, ep, address, data_valid_count=data_valid_count)
-    packets.append(TxDataPacket(rand, data_start_val=dataval, data_valid_count=data_valid_count, length=10, pid=0x3)) #DATA0
-    packets.append(RxHandshakePacket(data_valid_count=data_valid_count))
+    session.add_event(UsbTransaction(session, deviceAddress=address, endpointNumber=ep, endpointType="BULK", direction= "OUT", dataLength=10))
 
     # tmp hack for xs2 - for xs2 the shim will throw away the short token and it will never be seen by the xCORE
-
     if arch == 'xs3':
         # Create a short token, only PID and 2nd byte 
-        shorttoken = TxPacket(pid=0xe1, data_bytes = [0x81], data_valid_count=data_valid_count, inter_pkt_gap=100)
-        packets.append(shorttoken)
+        shorttoken = TxPacket(pid=USB_PID["OUT"], data_bytes = [0x81], data_valid_count=data_valid_count, interEventDelay=100)
+        session.add_event(shorttoken)
 
     #Finish with valid transaction 
-    dataval += 10
-    AppendOutToken(packets, ep, address, data_valid_count=data_valid_count, inter_pkt_gap=6000)
-    packets.append(TxDataPacket(rand, data_start_val=dataval, data_valid_count=data_valid_count, length=11, pid=0xb)) #DATA1
-    packets.append(RxHandshakePacket(data_valid_count=data_valid_count))
+    session.add_event(UsbTransaction(session, deviceAddress=address, endpointNumber=ep, endpointType="BULK", direction= "OUT", dataLength=11, interEventDelay=6000))
 
-    do_usb_test(arch, clk, phy, usb_speed, packets, __file__, seed, level='smoke', extra_tasks=[])
+    do_usb_test(arch, clk, phy, usb_speed, [session], __file__, seed, level='smoke', extra_tasks=[])
 
 def runtest():
-    random.seed(1)
-    runall_rx(do_test)
+    RunUsbTest(do_test)

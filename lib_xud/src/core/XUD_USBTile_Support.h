2774a30b56c092c7c6f98f3b8d7402d4d8f4bdc1
Ross Owen <ross@xmos.com>
2021-05-12 11:55:22 +0100

    Update XUD_USBTile_Support.h

--- lib_xud/src/core/XUD_USBTile_Support.h
+++ lib_xud/src/core/XUD_USBTile_Support.h
@@ -16,7 +16,9 @@ int read_periph_word(tileref tile, unsigned peripheral, unsigned addr, unsigned 
 
 unsigned get_tile_id(tileref ref);
 
+#ifdef __XC__
 void write_periph_word_two_part_start(chanend tmpchan, tileref tile, unsigned peripheral,
                                               unsigned base_address, unsigned data);
 void write_periph_word_two_part_end(chanend tmpchan, unsigned data);
+#endif
 #endif

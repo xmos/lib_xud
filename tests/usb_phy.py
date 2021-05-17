30b4c472a8cac05e06d788141c32ee07c13e1f48
Kevin Yeung <kevinyeung@xmos.com>
2021-05-17 15:58:23 +0800

    remove comment

--- tests/usb_phy.py
+++ tests/usb_phy.py
@@ -1,7 +1,6 @@
 # Copyright 2016-2021 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 import random
-# import xmostest
 import Pyxsim
 import sys
 import zlib

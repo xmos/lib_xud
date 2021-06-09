09620fdebc3e2d0f7ca74c30766aed42cdc92c2d
xross <ross@xmos.com>
2021-06-09 14:58:15 +0100

    Stall test renamed. Added additional stall test.

---
+++ tests/test_stall_out_basic.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Copyright 2021 XMOS LIMITED.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
+
+import xmostest
+from usb_packet import *
+import usb_packet
+from helpers import do_usb_test, RunUsbTest
+from usb_session import UsbSession
+from usb_transaction import UsbTransaction
+
+
+def do_test(arch, clk, phy, usb_speed, seed, verbose=False):
+
+    address = 1
+    pktLength = 10
+
+    session = UsbSession(
+        bus_speed=usb_speed, run_enumeration=False, device_address=address
+    )
+
+    ep_ctrl = 3
+    ep = 2
+
+    session.add_event(
+        UsbTransaction(
+            session,
+            deviceAddress=address,
+            endpointNumber=ep,
+            endpointType="BULK",
+            direction="OUT",
+            dataLength=pktLength,
+            halted=True,
+        )
+    )
+
+    session.add_event(
+        UsbTransaction(
+            session,
+            deviceAddress=address,
+            endpointNumber=ep,
+            endpointType="BULK",
+            direction="OUT",
+            dataLength=pktLength,
+            halted=True,
+        )
+    )
+
+    session.add_event(
+        UsbTransaction(
+            session,
+            deviceAddress=address,
+            endpointNumber=ep_ctrl,
+            endpointType="BULK",
+            direction="OUT",
+            dataLength=pktLength,
+        )
+    )
+
+    do_usb_test(
+        arch,
+        clk,
+        phy,
+        usb_speed,
+        [session],
+        __file__,
+        seed,
+        level="smoke",
+        extra_tasks=[],
+        verbose=verbose,
+    )
+
+
+def runtest():
+    RunUsbTest(do_test)

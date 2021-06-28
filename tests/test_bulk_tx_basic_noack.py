741a649fe1c42e0b2235a5c4b8831aee125d82d9
xross <ross@xmos.com>
2021-06-28 17:11:30 +0100

    Resolving merge issues

--- tests/test_bulk_tx_basic_noack.py
+++ tests/test_bulk_tx_basic_noack.py
@@ -1,64 +1,51 @@
-#!/usr/bin/env python
 # Copyright 2016-2021 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
-
-import random
-import xmostest
-from  usb_packet import *
-#import * AppendSetupToken, TxDataPacket, RxDataPacket, TokenPacket, RxHandshakePacket, TxHandshakePacket
-from usb_clock import Clock
-from helpers import do_rx_test, packet_processing_time, get_dut_address
-from helpers import choose_small_frame_size, check_received_packet, runall_rx
+from usb_packet import USB_PID, TokenPacket, RxDataPacket
+from usb_session import UsbSession
+from usb_transaction import UsbTransaction
+import pytest
+from conftest import PARAMS, test_RunUsbSession
 
 
-# Single, setup transaction to EP 0
+@pytest.fixture
+def test_session(ep, address, bus_speed):
 
-def do_test(arch, tx_clk, tx_phy, seed):
-    rand = random.Random()
-    rand.seed(seed)
+    # Note, quite big gap to allow checking
+    ied = 4000
 
-    dev_address = get_dut_address()
-    ep = 2
+    session = UsbSession(
+        bus_speed=bus_speed, run_enumeration=False, device_address=address
+    )
 
-    # The inter-frame gap is to give the DUT time to print its output
-    packets = []
+    for pktLength in range(10, 14):
 
-    dataval = 0;
+        if pktLength == 12:
+            session.add_event(
+                TokenPacket(
+                    pid=USB_PID["IN"],
+                    address=address,
+                    endpoint=ep,
+                    interEventDelay=ied,
+                )
+            )
+            session.add_event(
+                RxDataPacket(
+                    dataPayload=session.getPayload_in(ep, pktLength, resend=True)
+                )
+            )
+            # Missing ACK - simulate CRC fail at host
 
-    AppendInToken(packets, ep)
-    packets.append(RxDataPacket(rand, data_start_val=dataval, length=10, pid=0x3)) #DATA0
-    packets.append(TxHandshakePacket())
+        session.add_event(
+            UsbTransaction(
+                session,
+                deviceAddress=address,
+                endpointNumber=ep,
+                endpointType="BULK",
+                direction="IN",
+                dataLength=pktLength,
+                interEventDelay=ied,
+            )
+        )
 
-    dataval += 10
-    AppendInToken(packets, ep, inter_pkt_gap=5000)
-    packets.append(RxDataPacket(rand, data_start_val=dataval, length=11, pid=0xb)) #DATA1
-    packets.append(TxHandshakePacket())
+    return session
 
-    #pretend CRC got broken on way to host and dont send ACK
-    dataval += 11
-    AppendInToken(packets, ep, inter_pkt_gap=5000)
-    packets.append(RxDataPacket(rand, data_start_val=dataval, length=12, pid=0x3)) #DATA0
-    #packets.append(TxHandshakePacket())
-
-    #xCore should resend same packet again..
-    AppendInToken(packets, ep, inter_pkt_gap=5000)
-    packets.append(RxDataPacket(rand, data_start_val=dataval, length=12, pid=0x3)) #DATA0
-    packets.append(TxHandshakePacket())
-
-    dataval += 12
-    AppendInToken(packets, ep, inter_pkt_gap=5000)
-    packets.append(RxDataPacket(rand, data_start_val=dataval, length=13, pid=0xb)) #DATA1
-    packets.append(TxHandshakePacket())
-
-    dataval += 13
-    AppendInToken(packets, ep, inter_pkt_gap=5000)
-    packets.append(RxDataPacket(rand, data_start_val=dataval, length=14, pid=0x3)) #DATA0
-    packets.append(TxHandshakePacket())
-
-    # Note, quite big gap to allow checking.
-    do_rx_test(arch, tx_clk, tx_phy, packets, __file__, seed,
-               level='smoke', extra_tasks=[])
-
-def runtest():
-    random.seed(1)
-    runall_rx(do_test)

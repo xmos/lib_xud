73943d3cf919758b953690d062b72b3c24210bb7
xross <ross@xmos.com>
2021-07-02 10:45:59 +0100

    Discard output from cleaning DUT bin

--- tests/Pyxsim/__init__.py
+++ tests/Pyxsim/__init__.py
@@ -7,7 +7,7 @@ This module provides functions to run tests for XMOS applications and libraries.
 """
 import sys
 import Pyxsim.pyxsim
-from Pyxsim.xmostest_subprocess import call_get_output, call
+from Pyxsim.xmostest_subprocess import call_get_output
 import platform
 import multiprocessing
 import tempfile
@@ -46,7 +46,7 @@ def _build(xe_path, build_config=None, env={}, do_clean=False, build_options="")
         cmd = ["xmake", "all"]
 
     if do_clean:
-        call(["xmake", "clean"], cwd=path, env=my_env)
+        clean_output = call_get_output(["xmake", "clean"], cwd=path, env=my_env)
 
     if build_config is not None:
         cmd += ["CONFIG=%s" % build_config]

345eae9087dc518aeeb5a1a15400ac91a41191c1
Dave Atkinson <davidatkinson@xmos.com>
2021-07-15 11:05:40 +0100

    Update copyright info in hid_defs.h.

--- examples/AN00129_hid_class/src/hid_defs.h
+++ examples/AN00129_hid_class/src/hid_defs.h
@@ -1,4 +1,9 @@
+// Copyright 2021 XMOS LIMITED.
+// This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
+/*
+ * @brief Defines shared data for HID example threads.
+ */
 #ifndef HID_DEFS_H
 #define HID_DEFS_H
 

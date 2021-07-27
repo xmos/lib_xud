f564cce609ceed6f7d071586ba34ba745008fed1
xross <ross@xmos.com>
2021-07-27 13:15:47 +0100

    Added lower freq XN files to copy list

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import shutil
 
 # Note, no current support for XS2 so don't copy XS2 xn files
-XN_FILES = ["test_xs3_600.xn", "test_xs3_800.xn"]
+XN_FILES = ["test_xs3_600.xn", "test_xs3_800.xn", "test_xs3_540.xn", "test_xs3_500.xn"]
 
 PARAMS = {
     "extended": {

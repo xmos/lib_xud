f3e3d4fb6f1ff699ba249d5332e5f3d9ebda13f7
xross <ross@xmos.com>
2021-08-10 16:15:29 +0100

    Increase test start up delay (fixes ping_stall test)

--- tests/usb_phy_utmi.py
+++ tests/usb_phy_utmi.py
@@ -17,7 +17,7 @@ class UsbPhyUtmi(UsbPhy):
         xcvrsel,
         termsel,
         clock,
-        initial_delay=63000,
+        initial_delay=64000,
         verbose=False,
         do_timeout=True,
         complete_fn=None,

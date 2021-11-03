e6ba6904a41bdeccc5b8e30ebdd3094fbc349c2f
Kevin Yeung <kevinyeung@xmos.com>
2021-11-03 19:20:07 +0800

    replace main.xc to c

--- examples/AN00129_hid_class/src/main.c
+++ examples/AN00129_hid_class/src/main.c
@@ -1,12 +1,24 @@
 // Copyright 2015-2021 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
-/*
- * @brief Implements endpoint zero for an example HID mouse device.
- */
-#include <xs1.h>
-#include "xud_device.h"
+
+#include <xcore/parallel.h>
+#include <xcore/chanend.h>
+#include <xcore/channel.h>
+
+#include <xud_device.h>
 #include "hid.h"
-#include "hid_defs.h"
+
+#include <stdio.h>
+
+/* Number of Endpoints used by this app */
+#define EP_COUNT_OUT   1
+#define EP_COUNT_IN    2
+
+/* Endpoint type tables - informs XUD what the transfer types for each Endpoint in use and also
+ * if the endpoint wishes to be informed of USB bus resets
+ */
+XUD_EpType epTypeTableOut[EP_COUNT_OUT] = { XUD_EPTYPE_CTL | XUD_STATUS_ENABLE };
+XUD_EpType epTypeTableIn[EP_COUNT_IN]   = { XUD_EPTYPE_CTL | XUD_STATUS_ENABLE, XUD_EPTYPE_BUL };
 
 /* USB HID Device Product Defines */
 #define BCD_DEVICE   0x1000
@@ -122,20 +134,22 @@ static unsigned char hidReportDescriptor[] =
     0xC0          /* End Collection */
 };
 
-unsafe{
+//unsafe{
 /* String table */
-static char * unsafe stringDescriptors[]=
+//static char * unsafe stringDescriptors[]=
+static char * stringDescriptors[]=
 {
     "\x09\x04",             // Language ID string (US English)
     "XMOS",                 // iManufacturer
     "Example HID Mouse",    // iProduct
     "Config",               // iConfiguration
 };
-}
+//}
 
 /* It is essential that HID_REPORT_BUFFER_SIZE, defined in hid_defs.h, matches the   */
 /* infered length of the report described in hidReportDescriptor above. In this case */
 /* it is three bytes, three button bits padded to a byte, plus a byte each for X & Y */
+#define HID_REPORT_BUFFER_SIZE 3u
 unsigned char g_reportBuffer[HID_REPORT_BUFFER_SIZE] = {0, 0, 0};
 
 /* HID Class Requests */
@@ -149,12 +163,15 @@ XUD_Result_t HidInterfaceClassRequests(XUD_ep c_ep0_out, XUD_ep c_ep0_in, USB_Se
 
             /* Mandatory. Allows sending of report over control pipe */
             /* Send a hid report - note the use of unsafe due to shared mem */
-            unsafe {
-              char * unsafe p_reportBuffer = g_reportBuffer;
-              buffer[0] = p_reportBuffer[0];
-            }
+            //unsafe {
+              //char * unsafe p_reportBuffer = g_reportBuffer;
+              //char * p_reportBuffer = g_reportBuffer;
+              //buffer[0] = p_reportBuffer[0];
+            //}
+            buffer[0] = g_reportBuffer[0];
 
-            return XUD_DoGetRequest(c_ep0_out, c_ep0_in, (buffer, unsigned char []), 4, sp.wLength);
+            //return XUD_DoGetRequest(c_ep0_out, c_ep0_in, (buffer, unsigned char []), 4, sp.wLength);
+            return XUD_DoGetRequest(c_ep0_out, c_ep0_in, (void*)buffer, 4, sp.wLength);
             break;
 
         case HID_GET_IDLE:
@@ -197,13 +214,17 @@ XUD_Result_t HidInterfaceClassRequests(XUD_ep c_ep0_out, XUD_ep c_ep0_in, USB_Se
     return XUD_RES_ERR;
 }
 
+DECLARE_JOB(Endpoint0, (chanend_t, chanend_t));
 /* Endpoint 0 Task */
-void Endpoint0(chanend chan_ep0_out, chanend chan_ep0_in)
+void Endpoint0(chanend_t chan_ep0_out, chanend_t chan_ep0_in)
 {
     USB_SetupPacket_t sp;
 
     unsigned bmRequestType;
     XUD_BusSpeed_t usbBusSpeed;
+
+    printf("Endpoint0: out: %x\n", chan_ep0_out);
+    printf("Endpoint0: in:  %x\n", chan_ep0_in);
 
     XUD_ep ep0_out = XUD_InitEp(chan_ep0_out);
     XUD_ep ep0_in  = XUD_InitEp(chan_ep0_in);
@@ -211,7 +232,8 @@ void Endpoint0(chanend chan_ep0_out, chanend chan_ep0_in)
     while(1)
     {
         /* Returns XUD_RES_OKAY on success */
-        XUD_Result_t result = USB_GetSetupPacket(ep0_out, ep0_in, sp);
+        //XUD_Result_t result = USB_GetSetupPacket(ep0_out, ep0_in, sp);
+        XUD_Result_t result = USB_GetSetupPacket(ep0_out, ep0_in, &sp);
 
         if(result == XUD_RES_OKAY)
         {
@@ -219,8 +241,8 @@ void Endpoint0(chanend chan_ep0_out, chanend chan_ep0_in)
             result = XUD_RES_ERR;
 
             /* Stick bmRequest type back together for an easier parse... */
-            bmRequestType = (sp.bmRequestType.Direction<<7) |
-                            (sp.bmRequestType.Type<<5) |
+            bmRequestType = (sp.bmRequestType.Direction << 7) |
+                            (sp.bmRequestType.Type << 5) |
                             (sp.bmRequestType.Recipient);
 
             if ((bmRequestType == USB_BMREQ_H2D_STANDARD_DEV) &&
@@ -284,18 +306,145 @@ void Endpoint0(chanend chan_ep0_out, chanend chan_ep0_in)
             /* Returns  XUD_RES_OKAY if handled okay,
              *          XUD_RES_ERR if request was not handled (STALLed),
              *          XUD_RES_RST for USB Reset */
-             unsafe{
+            //unsafe{
             result = USB_StandardRequests(ep0_out, ep0_in, devDesc,
                         sizeof(devDesc), cfgDesc, sizeof(cfgDesc),
-                        null, 0, null, 0, stringDescriptors, sizeof(stringDescriptors)/sizeof(stringDescriptors[0]),
-                        sp, usbBusSpeed);
-             }
+                        //null, 0, null, 0, stringDescriptors, sizeof(stringDescriptors)/sizeof(stringDescriptors[0]),
+                        NULL, 0, NULL, 0, stringDescriptors, sizeof(stringDescriptors)/sizeof(stringDescriptors[0]),
+                        //sp, usbBusSpeed);
+                        &sp, usbBusSpeed);
+             //}
         }
 
         /* USB bus reset detected, reset EP and get new bus speed */
         if(result == XUD_RES_RST)
         {
-            usbBusSpeed = XUD_ResetEndpoint(ep0_out, ep0_in);
+            //usbBusSpeed = XUD_ResetEndpoint(ep0_out, ep0_in);
+            usbBusSpeed = XUD_ResetEndpoint(ep0_out, &ep0_in);
         }
     }
+}
+
+DECLARE_JOB(hid_mouse, (chanend_t));
+/*
+ * This function responds to the HID requests 
+ * - It draws a square using the mouse moving 40 pixels in each direction
+ * - The sequence repeats every 500 requests.
+ */
+void hid_mouse(chanend_t chan_ep_hid)
+{
+    unsigned int counter = 0;
+    enum {RIGHT, DOWN, LEFT, UP} state = RIGHT;
+    
+    printf("hid_mouse: %x\n", chan_ep_hid);
+    XUD_ep ep_hid = XUD_InitEp(chan_ep_hid);
+
+    for(;;)
+    {
+        /* Move the pointer around in a square (relative) */
+        if(counter++ >= 500)
+        {
+            int x;
+            int y;
+
+            switch(state) {
+            case RIGHT:
+                x = 40;
+                y = 0;
+                state = DOWN;
+                break;
+
+            case DOWN:
+                x = 0;
+                y = 40;
+                state = LEFT;
+                break;
+
+            case LEFT:
+                x = -40;
+                y = 0;
+                state = UP;
+                break;
+
+            case UP:
+            default:
+                x = 0;
+                y = -40;
+                state = RIGHT;
+                break;
+            }
+
+            /* Unsafe region so we can use shared memory. */
+            //unsafe {
+                /* global buffer 'g_reportBuffer' defined in hid_defs.h */
+                //char * unsafe p_reportBuffer = g_reportBuffer;
+                
+                //p_reportBuffer[1] = x;
+                //p_reportBuffer[2] = y;
+                g_reportBuffer[1] = x;
+                g_reportBuffer[2] = y;
+
+                /* Send the buffer off to the host.  Note this will return when complete */
+                //XUD_SetBuffer(ep_hid, (char *) p_reportBuffer, sizeof(g_reportBuffer));
+                XUD_SetBuffer(ep_hid, (void*)g_reportBuffer, sizeof(g_reportBuffer));
+                counter = 0;
+            //}
+        }
+    }
+}
+
+DECLARE_JOB(_XUD_Main, (chanend_t*, int, chanend_t*, int, chanend_t, XUD_EpType*, XUD_EpType*, XUD_BusSpeed_t, XUD_PwrConfig));
+//DECLARE_JOB(XUD_Main, (chanend, chanend, NULLABLE_RESOURCE(chanend, c_sof), XUD_EpType, XUD_EpType, XUD_BusSpeed_t, XUD_PwrConfig));
+void _XUD_Main(chanend_t *c_epOut, int noEpOut, chanend_t *c_epIn, int noEpIn, chanend_t c_sof, XUD_EpType *epTypeTableOut, XUD_EpType *epTypeTableIn, XUD_BusSpeed_t desiredSpeed, XUD_PwrConfig pwrConfig)
+{
+    for(int i = 0; i < EP_COUNT_OUT; ++i) {
+        printf("out[%d]: %x\n", i, c_epOut[i]);
+    }
+    for(int i = 0; i < EP_COUNT_IN; ++i) {
+        printf("in[%d]: %x\n", i, c_epIn[i]);
+    }
+    XUD_Main(c_epOut, noEpOut, c_epIn, noEpIn, c_sof,
+        epTypeTableOut, epTypeTableIn, desiredSpeed, pwrConfig);
+}
+
+int main()
+{
+    channel_t channel_ep_out[EP_COUNT_OUT];
+    channel_t channel_ep_in[EP_COUNT_IN];
+
+    for(int i = 0; i < sizeof(channel_ep_out) / sizeof(*channel_ep_out); ++i) {
+        channel_ep_out[i] = chan_alloc();
+    }
+    for(int i = 0; i < sizeof(channel_ep_in) / sizeof(*channel_ep_in); ++i) {
+        channel_ep_in[i] = chan_alloc();
+    }
+
+    chanend_t c_ep_out[EP_COUNT_OUT];
+    chanend_t c_ep_in[EP_COUNT_IN];
+
+    for(int i = 0; i < EP_COUNT_OUT; ++i) {
+        c_ep_out[i] = channel_ep_out[i].end_a;
+    }
+    for(int i = 0; i < EP_COUNT_IN; ++i) {
+        c_ep_in[i] = channel_ep_in[i].end_a;
+    }
+
+    /*
+        on tile[0]: XUD_Main(c_ep_out, EP_COUNT_OUT, c_ep_in, EP_COUNT_IN, null,
+                             epTypeTableOut, epTypeTableIn, XUD_SPEED_HS, XUD_PWR_BUS);
+
+        on tile[0]: Endpoint0(c_ep_out[0], c_ep_in[0]);
+
+        
+        on tile[0]: hid_mouse(c_ep_in[1]);
+        */
+    PAR_JOBS(
+        PJOB(_XUD_Main, (c_ep_out, EP_COUNT_OUT, c_ep_in, EP_COUNT_IN, 0, epTypeTableOut, epTypeTableIn, XUD_SPEED_HS, XUD_PWR_BUS)),
+        //PJOB(Endpoint0, (c_ep_out[0], c_ep_in[0])),
+        //PJOB(hid_mouse, (c_ep_in[1]))
+        PJOB(Endpoint0, (channel_ep_out[0].end_b, channel_ep_in[0].end_b)),
+        PJOB(hid_mouse, (channel_ep_in[1].end_b))
+    );
+
+    return 0;
 }

9ac4024fbe06e90b83217b50dba43a93606d5465
xross <ross@xmos.com>
2022-01-12 13:13:23 +0000

    Added TxRdy pulsing to TB

--- tests/usb_packet.py
+++ tests/usb_packet.py
@@ -255,12 +255,18 @@ class RxPacket(UsbPacket):
                 in_rx_packet = True
                 break
 
+        txrdy_pulse = USB_DATA_VALID_COUNT[bus_speed] - 1
+
         if not in_rx_packet:
             print("ERROR: Timed out waiting for packet")
         else:
             while in_rx_packet:
 
-                # TODO txrdy pulsing
+                # Tx Rdy pulsing
+                for i in range(txrdy_pulse):
+                    wait(lambda x: usb_phy._clock.is_high())
+                    wait(lambda x: usb_phy._clock.is_low())
+
                 xsi.drive_port_pins(usb_phy._txrdy, 1)
                 data = xsi.sample_port_pins(usb_phy._txd)
 
@@ -269,6 +275,9 @@ class RxPacket(UsbPacket):
 
                 wait(lambda x: usb_phy._clock.is_high())
                 wait(lambda x: usb_phy._clock.is_low())
+
+                # Note, for HS this will be set high again before another clock
+                xsi.drive_port_pins(usb_phy._txrdy, 0)
 
                 if xsi.sample_port_pins(usb_phy._txv) == 0:
                     # TxV low, break out of loop

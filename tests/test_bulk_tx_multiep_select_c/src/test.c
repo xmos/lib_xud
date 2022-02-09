2640b9af1d8b93cdb4d77a5b610d18c90fcec21c
Ross Owen <ross@xmos.com>
2022-02-09 09:42:26 +0000

    Test/select c tx (#324)
    * XUD_SetReady_In now builds in C
    * Added volatile to various inline asm
    * Added test_bulk_tx_multiep_select_c
    * Increased packet count of test_bulk_tx_multiep_select
    * Build fix to test_control_basic_get_epmax

--- tests/test_bulk_tx_multiep_select_c/src/test.c
+++ tests/test_bulk_tx_multiep_select_c/src/test.c
@@ -1,9 +1,8 @@
 // Copyright 2022 XMOS LIMITED.
 // This Software is subject to the terms of the XMOS Public Licence: Version 1.
+#include <xcore/select.h>
+#include "test.h"
 #include "shared.h"
-
-#define EP_COUNT_OUT       (7)
-#define EP_COUNT_IN        (7)
 
 #define PACKET_LEN_START   (10)
 #define PACKET_LEN_END     (19)
@@ -25,10 +24,6 @@
 
 #define TEST_EP_COUNT      (3)
 
-/* Endpoint type tables */
-XUD_EpType epTypeTableOut[EP_COUNT_OUT] = {XUD_EPTYPE_CTL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL};
-XUD_EpType epTypeTableIn[EP_COUNT_IN] =   {XUD_EPTYPE_CTL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL, XUD_EPTYPE_BUL};
-
 unsigned test_func(chanend c_ep_out[EP_COUNT_OUT], chanend c_ep_in[EP_COUNT_IN])
 {
     unsigned char buffer[PACKET_COUNT][512];
@@ -38,7 +33,6 @@ unsigned test_func(chanend c_ep_out[EP_COUNT_OUT], chanend c_ep_in[EP_COUNT_IN])
     unsigned pktLength[TEST_EP_COUNT];
     unsigned bufferIndex[TEST_EP_COUNT];
 
-#pragma unsafe arrays
     for(size_t i = 0; i< TEST_EP_COUNT; i++)
     {
         pktLength[i] = PACKET_LEN_START;
@@ -49,7 +43,6 @@ unsigned test_func(chanend c_ep_out[EP_COUNT_OUT], chanend c_ep_in[EP_COUNT_IN])
     unsigned length = PACKET_LEN_START;
 
     /* Create the packets we are going to send */
-#pragma unsafe arrays
     for(size_t i = 0; i < PACKET_COUNT; i++)
     {
         for(size_t j = 0; j < length; j++)
@@ -60,43 +53,47 @@ unsigned test_func(chanend c_ep_out[EP_COUNT_OUT], chanend c_ep_in[EP_COUNT_IN])
     } 
 
     XUD_ep ep_in0 = XUD_InitEp(c_ep_in[TEST_EP_NUM]);
-    XUD_SetReady_In(ep_in0, buffer[bufferIndex[0]++], pktLength[0]++);
-    
     XUD_ep ep_in1 = XUD_InitEp(c_ep_in[TEST_EP_NUM+1]);
-    XUD_SetReady_In(ep_in1, buffer[bufferIndex[1]++], pktLength[1]++);
-    
     XUD_ep ep_in2 = XUD_InitEp(c_ep_in[TEST_EP_NUM+2]);
-    XUD_SetReady_In(ep_in2, buffer[bufferIndex[2]++], pktLength[2]++);
 
+    XUD_SetReady_In(ep_in0, buffer[bufferIndex[0]++], pktLength[0]++);
+    XUD_SetReady_In(ep_in1, buffer[bufferIndex[1]++], pktLength[1]++);
+    XUD_SetReady_In(ep_in2, buffer[bufferIndex[2]++], pktLength[2]++);
+    
     while(!exit)
     {
-        select
+        SELECT_RES(
+                CASE_THEN(c_ep_in[TEST_EP_NUM], ep_a), 
+                CASE_THEN(c_ep_in[TEST_EP_NUM+1], ep_b), 
+                CASE_THEN(c_ep_in[TEST_EP_NUM+2], ep_c))
         {
-            case XUD_SetData_Select(c_ep_in[TEST_EP_NUM], ep_in0, result):
+            ep_a: 
+                XUD_SetData_Select(c_ep_in[TEST_EP_NUM], ep_in0, &result);
                 XUD_SetReady_In(ep_in0, buffer[bufferIndex[0]++], pktLength[0]++);
                 break;
-        
-            case XUD_SetData_Select(c_ep_in[TEST_EP_NUM+1], ep_in1, result):
+
+            ep_b: 
+                XUD_SetData_Select(c_ep_in[TEST_EP_NUM+1], ep_in1, &result);
                 XUD_SetReady_In(ep_in1, buffer[bufferIndex[1]++], pktLength[1]++);
                 break;
 
-            case XUD_SetData_Select(c_ep_in[TEST_EP_NUM+2], ep_in2, result):
+             ep_c: 
+                XUD_SetData_Select(c_ep_in[TEST_EP_NUM+2], ep_in2, &result);
                 XUD_SetReady_In(ep_in2, buffer[bufferIndex[2]++], pktLength[2]++);
                 break;
         }
-        
+    
         exit = 1;
-        for (size_t i = 0; i < 3; i++)
+        for (size_t i = 0; i < TEST_EP_COUNT; i++)
         {
-            if(pktLength[i] <= PACKET_LEN_END + 1)
+            if(pktLength[i] <= (PACKET_LEN_END + 1))
             {
                 exit = 0;
             }
         }
     }
-
+    
     // TODO do we need to do any checking?
     return 0;
 }
 
-#include "test_main.xc"

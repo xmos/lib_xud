0cee0402cc88e2f705d4a086a2ecbbe7b059ac21
xross <ross@xmos.com>
2022-07-27 11:39:16 +0100

    Copyright comment updates

--- tests/test_ping_rx_basic.py
+++ tests/test_ping_rx_basic.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright 2016-2021 XMOS LIMITED.
+# Copyright 2016-2022 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
 # Basic check of PING functionality

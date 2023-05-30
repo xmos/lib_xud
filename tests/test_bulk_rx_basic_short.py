acc8f152f0eab0cd3571feab8f60bd6df821ad96
Ross Owen <ross@xmos.com>
2023-05-30 14:32:37 +0100

    Add test_bulk_rx_basic_short

---
+++ tests/test_bulk_rx_basic_short.py
@@ -0,0 +1,34 @@
+# Copyright 2016-2025 XMOS LIMITED.
+# This Software is subject to the terms of the XMOS Public Licence: Version 1.
+import pytest
+
+from conftest import PARAMS, test_RunUsbSession  # noqa F401
+from usb_session import UsbSession
+from usb_transaction import UsbTransaction
+
+
+@pytest.fixture
+def test_session(ep, address, bus_speed):
+
+    start_length = 0
+    end_length = 11
+    interEventDelay = 100
+
+    session = UsbSession(
+        bus_speed=bus_speed, run_enumeration=False, device_address=address
+    )
+
+    for pktLength in range(start_length, end_length):
+        session.add_event(
+            UsbTransaction(
+                session,
+                deviceAddress=address,
+                endpointNumber=ep,
+                endpointType="BULK",
+                transType="OUT",
+                dataLength=pktLength,
+                interEventDelay=interEventDelay,
+            )
+        )
+
+    return session

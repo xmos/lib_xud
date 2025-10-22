d2673d16a669575c3a027e54c8a88246b9b4fb77
Ross Owen <ross@xmos.com>
2025-10-22 11:05:38 +0100

    Update comments for USB bus reset handling

--- examples/app_hid_mouse/src/main.c
+++ examples/app_hid_mouse/src/main.c
@@ -17,7 +17,7 @@
 #define EP_COUNT_IN    2
 
 /* Endpoint type tables - informs XUD what the transfer types for each Endpoint in use and also
- * if the endpoint wishes to be informed of USB bus resets
+ * if the endpoint wishes to be informed of USB bus status updates.
  */
 XUD_EpType epTypeTableOut[EP_COUNT_OUT] = { XUD_EPTYPE_CTL | XUD_STATUS_ENABLE };
 XUD_EpType epTypeTableIn[EP_COUNT_IN]   = { XUD_EPTYPE_CTL | XUD_STATUS_ENABLE, XUD_EPTYPE_BUL };
@@ -162,7 +162,7 @@ void Endpoint0(chanend_t chan_ep0_out, chanend_t chan_ep0_in)
                     {
                         /* Returns  XUD_RES_OKAY if handled,
                          *          XUD_RES_ERR if not handled,
-                         *          XUD_RES_UPDATE for bus reset */
+                         *          XUD_RES_UPDATE for bus state update */
                         result = HidInterfaceClassRequests(ep0_out, ep0_in, sp);
                     }
                     break;
